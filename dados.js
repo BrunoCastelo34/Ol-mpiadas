606d705e8f4862424c23da671ec9651a49349063
BrunoCastelo34 <castelobruno24@gmail.com>
2024-09-06 14:21:35 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    },
+    {
+        titulo : "Beatriz Souza",
+        descricao : "Beatriz Souza é uma judoca brasileira que se destacou em diversas competições internacionais. Sua técnica e determinação a tornaram uma das principais atletas do judô brasileiro.",
+        link : "https://www.cbj.com.br/atletas/beatriz-souza/",
+        tags: "judô judo judoca judoka" // Substitua por um link real para o perfil da Beatriz Souza
+    },
+    {
+        titulo : "Rebeca Andrade",
+        descricao : "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com suas performances impecáveis. Ela se tornou um ícone da ginástica artística no país.",
+        link : "https://www.cbg.org.br/atletas/rebeca-andrade/",
+        tags: "ginástica ginastica ouro" // Substitua por um link real para o perfil da Rebeca Andrade
+    }
+];
